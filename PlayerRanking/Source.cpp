1058350f8465127fe432b143dfcf18786c7afb07
marcardilamillan <dj.marc14@gmail.com>
2017-04-06 11:27:45 +0200

    asdf

---
+++ PlayerRanking/Source.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <map>
+#include <string>
+#include <iostream>
+
+
+void main(){
+
+	std::map <std::string, int>mn;
+
+	mn["Paco"] = { 19 };
+	mn["Luis"] = { 22 };
+	mn["Pepe"] = { 18 };
+	mn["Maria"] = { 17 };
+
+	
+}
