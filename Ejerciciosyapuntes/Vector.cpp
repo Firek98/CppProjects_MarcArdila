601fb5fd59390e1d64f81ad20ea1c972658636a1
Firek98 <dj.marc14@gmail.com>
2017-04-11 13:46:59 +0200

    Vector en proceso

---
+++ Ejerciciosyapuntes/Vector.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+
+// 1 - Declarar e inicializar los siguientes conjuntos de elementos haciendo uso del contenedor vector de la librería STL.
+
+//	v1: vector de 5 elementos de tipo de entero sin inicializar.
+
+std::vector<int>v1(5);
+
+//  v2: vector de 10 elementos de tipo entero inicializados en 0.
+
+std::vector<int>v2(10);
+
+//  v3: {10,9,8,7,6,5,4,3,2,1}
+
+std::vector<int>v3({ 10,9,8,7,6,5,4,3,2,1 });
+
+//  v4: {"Gertrudiz", "Pancracia", "Anacleto", "Hipolito", "Eustaquio", "Fulgencia"};
+
+//  v5: Vector que contiene la misma información que v3.
+
+//  v6: vector que contiene la información de personas. Para cada persona se tiene el nombre, la fecha de nacimiento 
+//		y su dni. Inicializarlo en el momento de la declaración con 3 personas con datos inventados.
