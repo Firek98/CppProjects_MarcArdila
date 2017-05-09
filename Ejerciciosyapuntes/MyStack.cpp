db2ef925169c4fdf11eb9741b1cc5a7709f33cdd
marcardilamillan <dj.marc14@gmail.com>
2017-05-09 13:44:57 +0200

    send nodos jaj

---
+++ Ejerciciosyapuntes/MyStack.cpp
@@ -0,0 +1,45 @@
+#include "MyStack.h"
+
+
+
+MyStack::MyStack()
+{
+}
+
+MyStack::MyStack(MyStack & m)
+{
+}
+
+MyStack::MyStack(int a, int b)
+{
+}
+
+
+MyStack::~MyStack()
+{
+}
+
+void MyStack::pop()
+{
+}
+
+void MyStack::push(int elem)
+{
+	node *t;
+	t = new node();
+}
+
+int MyStack::top()
+{
+	return 0;
+}
+
+int MyStack::size()
+{
+	return 0;
+}
+
+bool MyStack::isEmpty()
+{
+	return false;
+}
